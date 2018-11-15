7563cdcc848a38efd6039a7602ae5e7f6c32ccd8
root <root@localhost.localdomain>
2018-11-15 10:55:57 +0900

    v2

--- dein_install.py
+++ dein_install.py
@@ -1,4 +1,6 @@
+#! /usr/bin/env python
+# coding: utf-8
+
 import subprocess as sp
 sp.call("mkdir -p ~/.cache/dein/repos/github.com/Shougo/dein.vim", shell=True)
 sp.call("git clone https://github.com/Shougo/dein.vim.git ~/.cache/dein/repos/github.com/Shougo/dein.vim", shell=True)
-
